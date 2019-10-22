996a1e4b205e3f13eb36a3a754a7fa54d76299b8
ismail hossain <ismailhossain.cuetcse13@gmail.com>
2019-10-22 22:44:15 +0600

    [Web-app] nav bar & side bar -added

---
+++ my-app/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './core/home/home.component';
+import { HeaderComponent } from './core/header/header.component';
+import { Route, RouterModule } from '@angular/router';
+
+const ROUTES: Route[] = [
+  { path: '', component: HomeComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
